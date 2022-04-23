c2cedfb8dbbb99b573dada9cc17e71936c19687c
TamannaSheme <syedatsheme03@gmail.com>
2022-04-23 21:48:04 +0600

    App Done

---
+++ app/src/test/java/edu/ewubd/loadcoursedatafromremoteserver/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package edu.ewubd.loadcoursedatafromremoteserver;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(10, 5 + 5);
+    }
+}
